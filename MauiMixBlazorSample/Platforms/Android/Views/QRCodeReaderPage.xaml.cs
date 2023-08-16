9f37edabef74c1ec6dfd0481397f8ecdea4bb097
infinith4 <infinith4@gmail.com>
2023-08-16 14:58:21 +0900

    add

---
+++ MauiMixBlazorSample/Platforms/Android/Views/QRCodeReaderPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace MauiMixBlazorSample.Platforms.Android.Views;
+
+public partial class QRCodeReaderPage
+{
+	public QRCodeReaderPage()
+	{
+		InitializeComponent();
+    }
+    private void scanner_BarcodesDetected(object sender, ZXing.Net.Maui.BarcodeDetectionEventArgs e)
+    {
+        scanner.IsDetecting = false;
+
+        Close(e.Results[0].Value);
+    }
+}
